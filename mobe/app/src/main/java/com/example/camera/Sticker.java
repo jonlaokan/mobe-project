ddce9539758b28f056ffdef02adf69a0948830e7
jonlaokan <oneofdays@gmail.com>
2020-03-07 16:48:58 +0100

    add sticker class

---
+++ mobe/app/src/main/java/com/example/camera/Sticker.java
@@ -0,0 +1,110 @@
+package com.example.camera;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+
+public class Sticker {
+
+    private Bitmap originalBitmap;
+    private Bitmap scaledBitmap;
+    private int height;
+    private int width;
+
+    private int xPos = 0, yPos = 0;
+    private int xOffset = 0, yOffset = 0;
+    private int defaultx, defaulty;
+
+    private boolean isTouched = false;
+
+    Sticker(Bitmap bitmap, int firstx, int firsty) {
+        setOriginalBitmap(bitmap);
+        defaultx = firstx;
+        defaulty = firsty;
+
+        setxPos(defaultx);
+        setyPos(defaulty);
+
+       setScaledBitmap(Bitmap.createScaledBitmap(bitmap, 400, 400, true));
+       setHeight(scaledBitmap.getHeight());
+       setWidth(scaledBitmap.getWidth());
+    }
+
+    public Bitmap getOriginalBitmap() {
+        return originalBitmap;
+    }
+
+    public void setOriginalBitmap(Bitmap originalBitmap) {
+        this.originalBitmap = originalBitmap;
+    }
+
+    public Bitmap getScaledBitmap() {
+        return scaledBitmap;
+    }
+
+    public void setScaledBitmap(Bitmap scaledBitmap) {
+        this.scaledBitmap = scaledBitmap;
+    }
+
+    public int getxPos() {
+        return xPos;
+    }
+
+    public void setxPos(int xPos) {
+        this.xPos = xPos;
+    }
+
+    public int getyPos() {
+        return yPos;
+    }
+
+    public void setyPos(int yPos) {
+        this.yPos = yPos;
+    }
+
+    public int getxOffset() {
+        return xOffset;
+    }
+
+    public void setxOffset(int xOffset) {
+        this.xOffset = xOffset;
+    }
+
+    public int getyOffset() {
+        return yOffset;
+    }
+
+    public void setyOffset(int yOffset) {
+        this.yOffset = yOffset;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public boolean isTouched() {
+        return isTouched;
+    }
+
+    public void setTouched(boolean touched) {
+        isTouched = touched;
+    }
+
+    public void reset() {
+        setxPos(defaultx);
+        setyPos(defaulty);
+    }
+
+}
