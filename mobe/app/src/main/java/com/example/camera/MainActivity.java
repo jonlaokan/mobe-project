0ed55c8454eb2e334ebb4e6b03b27cabad7b23a9
jonlaokan <oneofdays@gmail.com>
2020-03-07 18:00:30 +0100

    add more modularity to StickerActivity

--- mobe/app/src/main/java/com/example/camera/MainActivity.java
+++ mobe/app/src/main/java/com/example/camera/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.camera;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,7 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class
 MainActivity extends AppCompatActivity {
 
-    private TouchAppView canvas;
+    private StickerView canvas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +19,7 @@ MainActivity extends AppCompatActivity {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
-        canvas = (TouchAppView) findViewById(R.id.sticker_canvas);
+        canvas = (StickerView) findViewById(R.id.sticker_canvas);
     }
 
     public void addDogSticker(View view) {
--- mobe/app/src/main/java/com/example/camera/StickerView.java
+++ mobe/app/src/main/java/com/example/camera/StickerView.java
@@ -1,51 +1,50 @@
 package com.example.camera;
 
 import android.content.Context;
-import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.Toast;
 
-public class TouchAppView extends View {
+import java.util.ArrayList;
+
+public class StickerView extends View {
 
     private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
     private float x, y;
     private boolean touching;
 
-    private Sticker dog;
-    private Sticker sun;
-
+    private ArrayList<Sticker> stickers = new ArrayList<>();
 
     private Context c;
-    public TouchAppView(Context context, AttributeSet attrs) {
+    public StickerView(Context context, AttributeSet attrs) {
         super(context, attrs);
         c = context;
         loadStickers();
     }
 
     private void loadStickers(){
+        Sticker dog;
+        Sticker sun;
         dog = new Sticker(BitmapFactory.decodeResource(getResources(),
                 R.drawable.dog_smiling), 528, 1827 );
 
         sun = new Sticker(BitmapFactory.decodeResource(getResources(),
                 R.drawable.sun), 1000, 1800);
+        stickers.add(dog);
+        stickers.add(sun);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
 
-        if (dog.shouldBeDrawn()) {
-           canvas.drawBitmap(dog.getScaledBitmap(), dog.getxPos(), dog.getyPos(), paint);
-        }
-        if (sun.shouldBeDrawn()) {
-            canvas.drawBitmap(sun.getScaledBitmap(), sun.getxPos(), sun.getyPos() , paint);
+        for (Sticker s : stickers) {
+            drawSticker(canvas,s);
         }
     }
+
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
@@ -61,20 +60,24 @@ public class TouchAppView extends View {
                 y = event.getY();
                 touching = true;
 
-                dragSticker(dog, (int) x, (int) y);
-                dragSticker(sun, (int) x, (int) y);
+                for (Sticker s : stickers) {
+                    dragSticker(s, (int) x, (int) y);
+                }
+
                 break;
             case MotionEvent.ACTION_MOVE:
                 x = event.getX();
                 y = event.getY();
                 touching = true;
 
-                dropSticker(dog, (int) x, (int) y);
-                dropSticker(sun, (int) x, (int) y);
+                for (Sticker s : stickers) {
+                    dropSticker(s, (int) x, (int) y);
+                }
                 break;
             default:
-                dog.setTouched(false);
-                sun.setTouched(false);
+                for (Sticker s : stickers) {
+                    s.setTouched(false);
+                }
                 touching = false;
         }
         // Redraw the canvas
@@ -102,19 +105,25 @@ public class TouchAppView extends View {
         }
     }
 
+    private void drawSticker(Canvas canvas,Sticker sticker) {
+        if (sticker.shouldBeDrawn())
+            canvas.drawBitmap(sticker.getScaledBitmap(), sticker.getxPos(), sticker.getyPos(), paint);
+    }
+
     public void addDogSticker(View view) {
-        dog.setisWanted(true);
+        stickers.get(0).setisWanted(true);
         invalidate();
     }
 
     public void addSunSticker(View view) {
-        sun.setisWanted(true);
+        stickers.get(1).setisWanted(true);
         invalidate();
     }
 
     public void clearStickers(View view) {
-        dog.reset();
-        sun.reset();
+        for (Sticker s : stickers) {
+            s.reset();
+        }
         invalidate();
     }
 }
