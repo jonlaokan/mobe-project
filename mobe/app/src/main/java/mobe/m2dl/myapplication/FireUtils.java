cc26dc99e8269e3b8b975f90a5c6a2482ef8f577
Manuel CABARCOS BAULINA <crac.master@hotmail.com>
2020-03-12 22:44:11 +0100

    wip

--- mobe/app/src/main/java/mobe/m2dl/myapplication/FireUtils.java
+++ mobe/app/src/main/java/mobe/m2dl/myapplication/FireUtils.java
@@ -1,40 +1,56 @@
 package mobe.m2dl.myapplication;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.location.Location;
+import android.media.ExifInterface;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageMetadata;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.Date;
 
 public class FireUtils {
-    void writeImage(String fileName ){
 
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
-        byte[] data = baos.toByteArray();
-        FirebaseStorage storage = FirebaseStorage.getInstance();
-        StorageReference r = storage.getReference("image/im_" + new Date().getTime()+ ".png");
 
-        UploadTask uploadTask = r.putBytes(data);
-        uploadTask.addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure( Exception exception) {
-                // use toastr function
-            }
-        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-            @Override
-            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                // taskSnapshot.getMetadata() contains file metadata such as size, content-type, etc.
-                // ...
+    void writeImage(String fileName, Location location, final Runnable onSuccess) throws IOException, IllegalArgumentException {
+            Bitmap bitmap = BitmapFactory.decodeFile(fileName);
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
+            byte[] data = baos.toByteArray();
+            FirebaseStorage storage = FirebaseStorage.getInstance();
+            StorageReference r = storage.getReference("image/im_" + new Date().getTime()+ ".png");
+            StorageMetadata storageMetadata = new StorageMetadata.Builder()
+                    .setCustomMetadata("lat",Double.toString(location.getLatitude()))
+                    .setCustomMetadata("long",Double.toString(location.getLongitude()))
+                    .build();
+
+            UploadTask uploadTask = r.putBytes(data,storageMetadata);
+            uploadTask.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure( Exception exception) {
+                    throw new RuntimeException();
                 }
-        });
+            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    // taskSnapshot.getMetadata() contains file metadata such as size, content-type, etc.
+                    // ...
+                    onSuccess.run();
+                }
+            });
 
-        r.putBytes("lol".getBytes());
+
+
+
+
     }
 ;
 }
--- mobe/app/src/main/java/mobe/m2dl/myapplication/MainActivity.java
+++ mobe/app/src/main/java/mobe/m2dl/myapplication/MainActivity.java
@@ -1,15 +1,29 @@
 package mobe.m2dl.myapplication;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
+    private FusedLocationProviderClient fusedLocationClient;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,13 +37,39 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        findViewById(R.id.afficher).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent myIntent = new Intent(MainActivity.this, MapsActivity.class);
-                MainActivity.this.startActivity(myIntent);
-            }
-        });
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                    4);
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(final Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            findViewById(R.id.afficher).setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    Intent myIntent = new Intent(MainActivity.this, MapsActivity.class);
+                                    try {
+                                        new FireUtils().writeImage(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)+"/scanner/lol.jpg",location, new Runnable() {
+                                            @Override
+                                            public void run() {
+                                            }
+                                        });
+                                    } catch (IOException e) {
+                                        e.printStackTrace();
+                                    }
+                                    MainActivity.this.startActivity(myIntent);
+                                }
+                            });
+
+                        }
+                    }
+                });
+
 
     }
+
+
 }
